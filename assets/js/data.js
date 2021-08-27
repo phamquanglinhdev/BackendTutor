168b4136e0c2bee5192a6ffe50ebe693cba1daf9
Phạm Quang Linh <77527768+phamquanglinhdev@users.noreply.github.com>
2021-08-27 16:09:58 +0700

    create data

---
+++ assets/js/data.js
@@ -0,0 +1,131 @@
+users = [
+    {
+        "user_id":"01",
+        "name":"Phạm Quang Linh",
+        "age":18,
+        "role":0,
+        "gender":1,
+        "fb":"fb.me/linhcuenini",
+        "status":1,
+        "course":{
+            "primary":true,
+            "secondary":true,
+            "high":true,
+        }
+    },
+    {
+        "user_id":"02",
+        "name":"Nguyễn Xuân Minh",
+        "age":21,
+        "role":1,
+        "gender":0,
+        "fb":"fb.me/xuanminhnguyen",
+        "status":1,
+        "course":{
+            "primary":true,
+            "secondary":true,
+            "high":false,
+        }
+    },
+    {
+        "user_id":"03",
+        "name":"Phạm Văn Trường",
+        "age":25,
+        "role":1,
+        "gender":1,
+        "fb":"fb.me/truongfpv001",
+        "status":0,
+        "course":{
+            "primary":false,
+            "secondary":false,
+            "high":true,
+        }
+    },
+    {
+        "user_id":"04",
+        "name":"Trần Đức Mạnh,
+        "age":26,
+        "role":1,
+        "gender":1,
+        "fb":"fb.me/manhnosts",
+        "status":1,
+        "course":{
+            "primary":false,
+            "secondary":true,
+            "high":true,
+        }
+    },
+    {
+        "user_id":"05",
+        "name":"Nguyễn Thị Linh Chi",
+        "age":18,
+        "role":0,
+        "gender":0,
+        "fb":"fb.me/linhchingox",
+        "status":1,
+        "course":{
+            "primary":true,
+            "secondary":true,
+            "high":true,
+        }
+    },
+    {
+        "user_id":"06",
+        "name":"Trần Cẩm Lan",
+        "age":18,
+        "role":0,
+        "gender":0,
+        "fb":"fb.me/camlamsux",
+        "status":1,
+        "course":{
+            "primary":true,
+            "secondary":true,
+            "high":true,
+        }
+    },
+    {
+        "user_id":"07",
+        "name":"Hoàng Văn Hùng",
+        "age":19,
+        "role":1,
+        "gender":1,
+        "fb":"fb.me/linhcuenini",
+        "status":1,
+        "course":{
+            "primary":false,
+            "secondary":false,
+            "high":true,
+        }
+    },
+
+];
+
+//user_id => mã người dùng
+//role => quyền  1: user ; 0 : admin
+//gender => giới tính 1: nam ; 0:nữ
+//course => khoá học
+
+courses = [
+    {
+        "name":"primary",
+        "lesson":40,
+        "joiner":45,
+        "teacher":"Pham Quang Linh"
+    },
+    {
+        "name":"secondary",
+        "lesson":40,
+        "joiner":12,
+        "teacher":"Pham Truong Giang"
+    },
+    {
+        "name":"high",
+        "lesson":40,
+        "joiner":5,
+        "teacher":"Pham Quang Linh"
+    },
+];
+//lesson: số buổi học
+//joiner: số ng tham gia
+
+
